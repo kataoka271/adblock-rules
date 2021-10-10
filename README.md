17894da778f468d77b5ad160a0540c17f54921a6
Hiroaki Kataoka <k_hir@hotmail.com>
2021-10-10 20:50:56 +0900

    add nanj-filter.txt

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 * [280blocker_adblock_filter.txt](https://github.com/kataoka271/280blocker/raw/main/280blocker_adblock_filter.txt)
 * [280blocker_domain_ag_filter.txt](https://github.com/kataoka271/280blocker/raw/main/280blocker_domain_ag_filter.txt)
+* [280blocker_adblock_filter_nanj.txt](https://github.com/kataoka271/280blocker/raw/main/280blocker_adblock_filter_nanj.txt)
--- update.py
+++ update.py
@@ -1,5 +1,10 @@
-import datetime
+from datetime import datetime
+import os.path
 import urllib.request
+
+
+def can_update(date, filename):
+    return not os.path.exists(filename) or (date - datetime.fromtimestamp(os.path.getmtime(filename))).days > 30
 
 
 def fetch(url, filename):
@@ -8,12 +13,32 @@ def fetch(url, filename):
             wf.write(rf.read())
 
 
+def concat(file1, file2, dst):
+    with open(file1, "r", encoding="utf-8") as f1:
+        with open(file2, "r", encoding="utf-8") as f2:
+            with open(dst, "w", encoding="utf-8") as wf:
+                wf.write(f1.read())
+                wf.write("\n")
+                wf.write(f2.read())
+
+
 def main():
-    today = datetime.datetime.today()
+    today = datetime.today()
     adblockUrl = "https://280blocker.net/files/280blocker_adblock_{:%Y%m}.txt".format(today)
     domainUrl = "https://280blocker.net/files/280blocker_domain_ag_{:%Y%m}.txt".format(today)
-    fetch(adblockUrl, "280blocker_adblock_filter.txt")
-    fetch(domainUrl, "280blocker_domain_ag_filter.txt")
+    nanjUrl = "https://raw.githubusercontent.com/nanj-adguard/nanj-filter/master/nanj-filter.txt"
+    adblockFile = "280blocker_adblock_filter.txt"
+    nanjFile = "nanj-filter.txt"
+    domainFile = "280blocker_domain_ag_filter.txt"
+    adblockNanjFile = "280blocker_adblock_filter_nanj.txt"
+    if can_update(today, adblockFile):
+        fetch(adblockUrl, adblockFile)
+    if can_update(today, domainFile):
+        fetch(domainUrl, domainFile)
+    if can_update(today, nanjFile):
+        fetch(nanjUrl, nanjFile)
+    if can_update(today, adblockNanjFile):
+        concat(adblockFile, nanjFile, adblockNanjFile)
 
 
 if __name__ == '__main__':
