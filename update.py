2e9b91cbb1c9206b107931b66079f07a6ee380c3
Hiroaki Kataoka <k_hir@hotmail.com>
2024-04-29 17:44:33 +0900

    modify User-Agent

--- update.py
+++ update.py
@@ -49,6 +49,7 @@ def _fetch(etags: dict[str, str], rule: Union[TextRule, HtmlRule]) -> Optional[r
         headers = {}
     else:
         headers = {"If-None-Match": etags[rule.key]}
+    headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0"
     r = requests.get(rule.url, headers=headers)
     print("{} {}: {}".format(r.status_code, r.reason, rule.url))
     if r.status_code == requests.codes.ok:
@@ -95,7 +96,9 @@ def parse_nanj_wiki(doc: str) -> tuple[str, str]:
 
 def merge(rule: StructuredRule):
     if any(r.updated for r in rule.rules):
-        open(rule.path, "w", encoding="utf-8").write("".join(open(r.path, "r", encoding="utf-8").read() for r in rule.rules))
+        open(rule.path, "w", encoding="utf-8").write(
+            "".join(open(r.path, "r", encoding="utf-8").read() for r in rule.rules)
+        )
         print("update: {}".format(rule.path))
 
 
@@ -105,37 +108,29 @@ def main():
     r280_adblock = TextRule(
         url="https://280blocker.net/files/280blocker_adblock_{:%Y%m}.txt".format(today),
         key="https://280blocker.net/files/280blocker_adblock_xxxxxx.txt",
-        path="dist/280blocker_adblock_filter.txt"
+        path="dist/280blocker_adblock_filter.txt",
     )
     nanj_filter = TextRule(
         url="https://raw.githubusercontent.com/nanj-adguard/nanj-filter/master/nanj-filter.txt",
         key="https://raw.githubusercontent.com/nanj-adguard/nanj-filter/master/nanj-filter.txt",
-        path="dist/nanj-filter.txt"
+        path="dist/nanj-filter.txt",
     )
     r280_domain = TextRule(
         url="https://280blocker.net/files/280blocker_domain_ag_{:%Y%m}.txt".format(today),
         key="https://280blocker.net/files/280blocker_domain_ag_xxxxxx.txt",
-        path="dist/280blocker_domain_ag_filter.txt"
+        path="dist/280blocker_domain_ag_filter.txt",
     )
     nanj_wiki = HtmlRule(
         url="https://wikiwiki.jp/nanj-adguard/%E3%81%AA%E3%82%93J%E6%8B%A1%E5%BC%B5%E3%83%95%E3%82%A3%E3%83%AB%E3%82%BF%E3%83%BC",
         key="https://wikiwiki.jp/nanj-adguard/%E3%81%AA%E3%82%93J%E6%8B%A1%E5%BC%B5%E3%83%95%E3%82%A3%E3%83%AB%E3%82%BF%E3%83%BC",
-        adblock=Rule(
-            path="dist/supplement_rules.txt"
-        ),
-        domain=Rule(
-            path="dist/DNS_rules.txt"
-        ),
-        parser=parse_nanj_wiki
+        adblock=Rule(path="dist/supplement_rules.txt"),
+        domain=Rule(path="dist/DNS_rules.txt"),
+        parser=parse_nanj_wiki,
     )
     adblock_rules = StructuredRule(
-        rules=[r280_adblock, nanj_filter, nanj_wiki.adblock],
-        path="dist/01_adblock_rules.txt"
+        rules=[r280_adblock, nanj_filter, nanj_wiki.adblock], path="dist/01_adblock_rules.txt"
     )
-    domain_ag_rules = StructuredRule(
-        rules=[r280_domain, nanj_wiki.domain],
-        path="dist/01_domain_ag_rules.txt"
-    )
+    domain_ag_rules = StructuredRule(rules=[r280_domain, nanj_wiki.domain], path="dist/01_domain_ag_rules.txt")
     try:
         etags = pickle.load(open(path_etags, "rb"))
     except IOError:
@@ -151,5 +146,5 @@ def main():
     pickle.dump(etags, open(path_etags, "wb"))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
